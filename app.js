45250fbb94b67f5541faed87b7861e9b418073e9
Jacob Sharkey <jacob@greenshark.io>
2017-01-13 02:47:41 -0900

    ...

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var path = require("path");
 var app = express();
 app.use(express.static(__dirname + "/public"));
 
+app.get('/', function (req, res) {
+  res.send('Dis some bullshit on shabat')
+})
 
-
-  // Initialize the app.
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
