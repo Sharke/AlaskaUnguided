7a40a392ebb11d9cd12e238358cfb16911ebf33c
Conor Green <thefslife@gmail.com>
2017-01-17 22:52:30 +0000

    api link fix

--- public/app/fact.component.js
+++ public/app/fact.component.js
@@ -4,7 +4,7 @@ angular.
     template: '{{$ctrl.fact}}',
     controller: function FaceController($http) {
       var self = this;
-      $http.get('http://localhost:8080/api/fact/random').then(function(response) {
+      $http.get('/api/fact/random').then(function(response) {
         
        self.fact = response.data;
       });
