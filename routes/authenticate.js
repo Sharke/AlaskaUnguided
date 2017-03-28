0739bbdc3bd40c14c5215185e47f9caa70218f89
Jacob Sharkey <jacob@greenshark.io>
2017-03-28 11:19:19 -0700

    fixed crash

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -4,21 +4,26 @@ var mongoose = require('mongoose');
 var user = require('../models/user.js');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
-/* GET /todos listing. */
-router.post('/login',jsonParser, function(req, res, next) {
-    var count = 1;
-    var u = req.body.u;
-    var p = req.body.p;
-   console.log("Post Recieved" + " ");
-   user.findOne({'email' : u, "hash" : p},function (err, user) {
-    if (err) return next(err);
-    res.send(user.key);
-  });
-});
+
 
 router.get('/',function(req,res,next) {
     res.redirect('/');
 })
+
+router.post('/login',jsonParser, function(req, res, next) {
+    var count = 1;
+    var u = req.body.u;
+    var p = req.body.p;
+   user.findOne({'email' : u, "hash" : p},function (err, user) {
+    if (err) return next(err);
+    if (user != null) {
+    res.send(user.key);
+    } else {
+        res.send("https://www.youtube.com/watch?v=cQ_b4_lw0Gg");
+    }
+  });
+});
+
 
 
 
