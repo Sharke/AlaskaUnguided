1115e66d3eeac93aef32e96c80cc5797ae58ae1b
Conor <thefslife@gmail.com>
2017-03-28 16:19:17 +0000

    login works by storing cookie if login successful

--- public/cms/js/cms.js
+++ public/cms/js/cms.js
@@ -1,19 +1,62 @@
-var cmsApp = angular.module('cmsApp', ["ngRoute", "ngAnimate"]);
-
+var cmsApp = angular.module('cmsApp', ["ngRoute", "ngAnimate", "Auth", "ngCookies"]);
 //Routing
-cmsApp.config(function ($routeProvider, $locationProvider) {
+cmsApp.config(['$locationProvider', '$routeProvider', '$httpProvider', function ($locationProvider, $routeProvider,  $httpProvider) {
   $locationProvider.hashPrefix('');
+        // $urlRouterProvider.otherwise('/home');
+     //   var interceptor = ['$location', '$q', function ($location, $q) {
+
+     //       function success(response) {
+     //           return response;
+     //       }
+
+     //       function error(response) {
+
+     //           if (response.status === 401) {
+      //              $location.path('/login');
+      //              return $q.reject(response);
+      //          }
+     //           else {
+      //              return $q.reject(response);
+      //          }
+     //       }
+
+     //       return function (promise) {
+     //           return promise.then(success, error);
+     //       };
+     //   }];
+
+     //   $httpProvider.responseInterceptors.push(interceptor);
   $routeProvider
-    .when('/', {
+    .when('/login', {
       templateUrl: 'views/_login.html',
       controller: 'loginController'
       })
+      .when('/dash', {
+      templateUrl: 'views/_dash.html',
+      controller: 'dashController'
+      })
       .otherwise('/', {
-      templateUrl: 'views/home.html',
-      controller: 'homeController'
+      templateUrl: 'views/_login.html',
+      controller: 'loginController'
     })
+ }]);
 
-});
+//On run of the app check for user
+cmsApp.run(['Auth', '$cookieStore', '$rootScope','$location', function run(Auth, $cookieStore, $rootScope, $location) {
+          $rootScope.$on('$routeChangeStart', function (eve,nt) {
+
+        if (!Auth.isLoggedIn()) {
+            console.log('DENY');
+            event.preventDefault();
+            $location.path('/login');
+        }
+        else {
+            console.log('ALLOW');
+            $location.path('/dash');
+        }
+    });
+            
+        }])
 
 //Directives
 cmsApp.directive('routeLoadingIndicator', function($rootScope){
@@ -34,121 +77,3 @@ cmsApp.directive('routeLoadingIndicator', function($rootScope){
   };
 });
 
-'use strict';
- 
-angular.service('AuthenticationService',
-  ['$http', '$cookieStore', '$rootScope', '$timeout',
-    function ($http, $cookieStore, $rootScope, $timeout) {
-      var service = {};
-      service.Login = function (username, password, callback) {
-        $http.post('/api/authenticate', { username: username, password: password })
-          .success(function (response) {
-            callback(response);
-          });
-      };
- 
-      service.SetCredentials = function (username, password) {
-        var authdata = Base64.encode(username + ':' + password);
- 
-        $rootScope.globals = {
-          currentUser: {
-            username: username,
-            authdata: authdata
-          }
-        };
- 
-        $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata; // jshint ignore:line
-        $cookieStore.put('globals', $rootScope.globals);
-      };
- 
-      service.ClearCredentials = function () {
-        $rootScope.globals = {};
-        $cookieStore.remove('globals');
-        $http.defaults.headers.common.Authorization = 'Basic ';
-      };
- 
-      return service;
-    }]).factory('Base64', function () {
-      /* jshint ignore:start */
- 
-      var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
- 
-      return {
-        encode: function (input) {
-          var output = "";
-          var chr1, chr2, chr3 = "";
-          var enc1, enc2, enc3, enc4 = "";
-          var i = 0;
- 
-          do {
-            chr1 = input.charCodeAt(i++);
-            chr2 = input.charCodeAt(i++);
-            chr3 = input.charCodeAt(i++);
- 
-            enc1 = chr1 >> 2;
-            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-            enc4 = chr3 & 63;
- 
-            if (isNaN(chr2)) {
-              enc3 = enc4 = 64;
-            } else if (isNaN(chr3)) {
-              enc4 = 64;
-            }
- 
-            output = output +
-              keyStr.charAt(enc1) +
-              keyStr.charAt(enc2) +
-              keyStr.charAt(enc3) +
-              keyStr.charAt(enc4);
-            chr1 = chr2 = chr3 = "";
-            enc1 = enc2 = enc3 = enc4 = "";
-          } while (i < input.length);
- 
-          return output;
-        },
- 
-        decode: function (input) {
-          var output = "";
-          var chr1, chr2, chr3 = "";
-          var enc1, enc2, enc3, enc4 = "";
-          var i = 0;
- 
-          // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-          var base64test = /[^A-Za-z0-9\+\/\=]/g;
-          if (base64test.exec(input)) {
-            window.alert("There were invalid base64 characters in the input text.\n" +
-              "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
-              "Expect errors in decoding.");
-          }
-          input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
- 
-          do {
-            enc1 = keyStr.indexOf(input.charAt(i++));
-            enc2 = keyStr.indexOf(input.charAt(i++));
-            enc3 = keyStr.indexOf(input.charAt(i++));
-            enc4 = keyStr.indexOf(input.charAt(i++));
- 
-            chr1 = (enc1 << 2) | (enc2 >> 4);
-            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-            chr3 = ((enc3 & 3) << 6) | enc4;
- 
-            output = output + String.fromCharCode(chr1);
- 
-            if (enc3 != 64) {
-              output = output + String.fromCharCode(chr2);
-            }
-            if (enc4 != 64) {
-              output = output + String.fromCharCode(chr3);
-            }
- 
-            chr1 = chr2 = chr3 = "";
-            enc1 = enc2 = enc3 = enc4 = "";
- 
-          } while (i < input.length);
- 
-          return output;
-        }
-      };
-    });  
- 
--- public/cms/js/controllers/loginController.js
+++ public/cms/js/controllers/loginController.js
@@ -1,7 +1,21 @@
-cmsApp.controller('loginController', ['$scope', '$http', '$log', '$rootScope', '$location', function ($scope, $http, $log, $rootScope, AuthenticationService) {
-    $scope.login = function () {
-        AuthenticationService.login($scope.uname, $scope.pword, function (res) {
-            
+cmsApp.controller('loginController', ['$scope', '$http', '$log', '$rootScope', '$location', '$cookieStore', 'Auth', function ($scope, $http, $log, $rootScope, $location, $cookieStore, Auth) {
+    $scope.login = function() {
+        $scope.payload = {};
+        //This needs to be a http request 
+        $http.post('/api/auth/', $scope.payload, function(res){
+            //Logic if login was valid, if so, set the user cookie, if not, throw up UI error..
+        }).then(function(){
+            //if API post was unsuccessful
         });
+        if($scope.uname == "CONOR" && $scope.pword == "CONOR"){
+            Auth.setUser("CONOR");
+            alert('ok');
+        }else{
+            
+        }
     }
+    $scope.get = function () {
+        $log.debug($cookieStore.get('cms.usr'));
+    }
+    
 }]);
