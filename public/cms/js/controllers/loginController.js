6b653a0e268e6f00a057b12ce76d473c1c4eeb25
Conor Green <thefslife@gmail.com>
2017-03-28 19:01:10 +0100

    login changes

--- public/cms/js/controllers/loginController.js
+++ public/cms/js/controllers/loginController.js
@@ -1,15 +1,27 @@
 cmsApp.controller('loginController', ['$scope', '$http', '$log', '$rootScope', '$location', '$cookieStore', 'Auth', function ($scope, $http, $log, $rootScope, $location, $cookieStore, Auth) {
-    $scope.login = function() {
-        $scope.payload = {};
+    $scope.busy = false;
+    $scope.isInvalid = false;
+
+    $scope.login = function () {
+        $scope.busy = true;
+        $scope.payload = {u: $scope.uname, p: Sha256.hash($scope.pword)};
         //This needs to be a http request 
-        $http.post('/api/auth/', $scope.payload, function(res){
-            //Logic if login was valid, if so, set the user cookie, if not, throw up UI error..
-        }).then(function(){
+        $http.post('/api/authenticate/login', $scope.payload, function(res){
+            if (res.data == Sha256.hash($scope.payload)) {
+                alert("Valid user");
+                Auth.setUser(res.data);
+            } else {
+                $log.debug(res.data);
+                $log.debug(Sha256.hash($scope.payload));
+            }
+        }).then(function (data) {
+            $log.error(data);
             //if API post was unsuccessful
-        });
+            });
+        
         if($scope.uname == "CONOR" && $scope.pword == "CONOR"){
-            Auth.setUser("CONOR");
-            alert('ok');
+            
+            alert();
         }else{
             
         }
